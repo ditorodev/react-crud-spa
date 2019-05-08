5ae8f445f8d1c1acf64f3f3bd014fe295757030f
Juan Di Toro <ditorojuanf@gmail.com>
2019-05-08 14:31:02 -0400

    Updates

---
+++ precache-manifest.a351aedb5b569845a3da8b7610fde43d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd26e9cba57345a144af9104b40470c6",
+    "url": "/react-crud-spa/index.html"
+  },
+  {
+    "revision": "eb457d36b255a7e007bc",
+    "url": "/react-crud-spa/static/css/main.1715c2f2.chunk.css"
+  },
+  {
+    "revision": "92b3ecfafd99ae6d7232",
+    "url": "/react-crud-spa/static/js/2.ff8ff4ac.chunk.js"
+  },
+  {
+    "revision": "eb457d36b255a7e007bc",
+    "url": "/react-crud-spa/static/js/main.1220cb80.chunk.js"
+  },
+  {
+    "revision": "cf5ca4af94ecaecb1779",
+    "url": "/react-crud-spa/static/js/runtime~main.42bf0f66.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a80c4452f7cce24a5ee16035469b51b2.js"
+  "/react-crud-spa/precache-manifest.a351aedb5b569845a3da8b7610fde43d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-crud-spa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
