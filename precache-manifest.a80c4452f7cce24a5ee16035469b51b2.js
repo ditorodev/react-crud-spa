18d67ac496ce62db7bd8f67695cc7df7b178ebed
Juan Di Toro <ditorojuanf@gmail.com>
2019-05-08 14:29:43 -0400

    Updates

---
+++ precache-manifest.a80c4452f7cce24a5ee16035469b51b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5b1667d1db0da1e2a1c9f5c8c9e6ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5583c52efcd52a0274b5",
+    "url": "/static/css/main.1715c2f2.chunk.css"
+  },
+  {
+    "revision": "92b3ecfafd99ae6d7232",
+    "url": "/static/js/2.ff8ff4ac.chunk.js"
+  },
+  {
+    "revision": "5583c52efcd52a0274b5",
+    "url": "/static/js/main.569ce90b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
