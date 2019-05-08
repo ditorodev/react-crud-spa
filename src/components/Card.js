31bdb0cb3f0418fb2a8e84715515baf17a75037c
Juan Di Toro <ditorojuanf@gmail.com>
2019-05-08 14:23:15 -0400

    Adding Delete and Edit contacts functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,24 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import './Card.scss'
 
-const CardComponent = ({name, profile_pic, fav_social, social_link }) => (
+
+const CardComponent = ({user, deleteUser, modifyUser}) => (
     <article className="card">
         <header>
-            <img src={profile_pic || 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1'} alt={name.first_name + ' ' + (name.last_name || '')}></img>
-            <h3>{name.first_name + ' ' + (name.last_name || '')}</h3>
+            <img src={'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1'} alt={user.first_name + ' ' + (user.last_name || '')}></img>
+            <h3>{user.first_name + ' ' + (user.last_name || '')}</h3>
         </header>
-        Hellooooo {name.first_name}
+        <div>
+            <p onClick={()=>deleteUser(user)}>Eliminar</p>
+            <p onClick={()=> modifyUser(Object.assign({},user))}>Editar</p>
+        </div>
         <footer>
-            <h3>Favorite Social Networks: </h3>
-            {fav_social.map((item, index) => {
-                return <a href={social_link ? social_link[index] : '#'}><i className={`fab fa-${item}`} id={index}></i></a>
-            })}
+            <a href={user.twitter}>
+                    <i className="fab fa-twitter"></i>
+            </a>
+            <a href={user.facebook}>
+                    <i className="fab fa-facebook"></i>
+            </a>
         </footer>
     </article>
 );
@@ -21,7 +27,7 @@ const CardComponent = ({name, profile_pic, fav_social, social_link }) => (
 CardComponent.propTypes = {
     name: PropTypes.object,
     profile_pic: PropTypes.string,
-    fav_social: PropTypes.array,
+    fav_social: PropTypes.object,
 };
 
 export default CardComponent;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,37 +1,181 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import './Contacts.scss';
 
 import CardComponent from './Card';
+import Modal from './Modal';
+import { tsImportEqualsDeclaration } from '../../node_modules/@babel/types';
+ 
 
 class Contacts extends Component {
     constructor(props, ...rest) {
         super(props, ...rest);
         this.state = {
-            contacts: [{
-              name: {
-              first_name: 'Juan',
-              last_name: 'Di Toro'
-              },
-              fav_social: ['twitter', 'facebook'],
-              social_link: ['']
-          },
-          {
-            name: {
-            first_name: 'Jose',
-            last_name: 'Di Toro'
+            newUser: {
+                first_name: '',
+                last_name: '',
+                twitter: '',
+                facebook: ''
             },
-            fav_social: ['twitter', 'facebook'],
-            social_link: ['']
-      
-        }]
-    };
+
+            showModal: false,
+            contacts: [],
+            modalSaveHandler: this.saveDataModal.bind(this),
+            // modalSaveHandler: this.saveDataModal.bind(this)
+        };
+
+
+        this.checkContactInState = this.checkContactInState.bind(this);
+        this.addContact = this.addContact.bind(this);
+        this.removeContact = this.removeContact.bind(this);
+        this.editContact = this.editContact.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.saveDataModal = this.saveDataModal.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.updateContact = this.updateContact.bind(this);
+    }
+    /**
+     * add new contact to state
+     * @param  {Object} contact contact object
+     * @returns {Boolean} true if added correctly, false if contact is already on state
+     */
+    addContact(contact) {
+
+        if(this.checkContactInState(contact)) return false;
+
+        const newContacts = this.state.contacts;
+        newContacts.push(contact);
+
+        this.setState({contacts: newContacts});
+        return true;
+    }
+    /**
+     * removes contact from state
+     * @param  {Object} contact contact object
+     */
+    removeContact(contact) {
+        const newContacts = this.state.contacts.filter(arr_contact => !_.isEqual(contact, arr_contact));
+        
+        this.setState({contacts: newContacts});
+    }
+    /**
+     * edits contact info
+     * @param  {Object} contact contact object
+     */
+    editContact(contact){
+        
+        this.setState({
+            newUser: contact,
+            oldUser: Object.assign({}, contact)
+        });
+        this.toggleModal();
+
+        this.setState({
+            modalSaveHandler: this.updateContact
+        });
+        
+    }
+    
+    updateContact() {
+        const newInfo = this.state.newUser;
+        const oldInfo = this.state.oldUser;
+
+        const newContacts = this.state.contacts;
+        let index = newContacts.findIndex(arr_contact => _.isEqual(arr_contact, oldInfo));
+        newContacts[index] = newInfo;
+
+        this.setState({contacts: newContacts});
+        this.toggleModal();
+
+    }
+
+    /**
+     * checks if contact is already in state
+     * @param  {Object} contact contact to check in state
+     * @returns {Boolean} true if there is a user, false if no user found
+     */
+    checkContactInState(contact) {
+        const res = this.state.contacts.filter(arr_contact => _.isEqual(arr_contact, contact));
+        return res.length >= 1;
+    }
+
+    toggleModal() {
+        this.setState(state => ({
+            showModal: !state.showModal
+        }));
+    }
+
+    saveDataModal() {
+        this.addContact(this.state.newUser);
+        this.setState(state => ({
+            newUser: {
+                first_name: '',
+                last_name: '',
+                twitter: '',
+                facebook: ''
+            }
+        }));
+        this.toggleModal();
+    }
+
+
+    addHttp(url) {
+        if (!/^(f|ht)tps?:\/\//i.test(url)) {
+        
+           url = "http://" + url;
+        }
+        return url;
+     }
+    handleChange(event) {
+        // event.preventDefault();
+        let newState = this.state.newUser;
+        switch (event.target.name){
+            case 'first_name': 
+                newState.first_name = event.target.value;
+                this.setState({newUser: newState});
+            break;
+            case 'last_name': 
+                newState.last_name = event.target.value;
+                this.setState({newUser: newState});
+
+            break;
+            case 'twitter': 
+                newState.twitter = this.addHttp(event.target.value);
+                this.setState({newUser: newState});
+
+            break;
+            case 'facebook': 
+                newState.facebook = this.addHttp(event.target.value);
+                this.setState({newUser: newState});
+
+            break;
+            default:
+            break;
+
+        }
     }
 
     render() {
         return (
-            <div className="contacts-container">
-                {this.state.contacts.map((item, key) =>{
-                    return (<CardComponent name={item.name} fav_social={item.fav_social} profile_pic={item.profile_pic} id={key}/>);
-                })}
+            <div>
+                <div className="contacts-container">
+                    {this.state.contacts.map((item, key) =>{
+                        return (<CardComponent user={item} key={key} deleteUser={this.removeContact} modifyUser={this.editContact}/>);
+                    })}
+                    <div className="card add-contact" >
+                        <button onClick={this.toggleModal}> 
+                            Añadir Nuevo Contacto
+                        </button>
+                    </div>
+                </div>
+                <Modal 
+                    user={this.state.newUser}
+                    closeHandler={this.toggleModal} 
+                    show={this.state.showModal} 
+                    saveHandler={this.state.modalSaveHandler}
+                    changeHandler={this.handleChange}
+                    > 
+                </Modal>
             </div>
         );
     }
